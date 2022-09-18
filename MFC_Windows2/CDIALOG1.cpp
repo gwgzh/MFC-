f3e3d917b3acf8b98a764319b1ef956195a02d78
gwgzh <106792945+gwgzh@users.noreply.github.com>
2022-09-19 00:06:42 +0800

    Add files via upload

---
+++ MFC_Windows2/CDIALOG1.cpp
@@ -0,0 +1,34 @@
+﻿// CDIALOG1.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "MFC_Windows2.h"
+#include "CDIALOG1.h"
+#include "afxdialogex.h"
+
+
+// CDIALOG1 对话框
+
+IMPLEMENT_DYNAMIC(CDIALOG1, CDialogEx)
+
+CDIALOG1::CDIALOG1(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CDIALOG1::~CDIALOG1()
+{
+}
+
+void CDIALOG1::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDIALOG1, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDIALOG1 消息处理程序
